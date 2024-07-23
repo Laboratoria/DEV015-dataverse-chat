401f0cfb52994cd5f0c5d31cf2e0f317a50f06b2
Angie <angiesamper18@gmail.com>
2024-07-23 13:39:14 -0400

    fix problems

--- src/views/Chat.js
+++
@@ -1,6 +0,0 @@
-export function Chat() {
-  const contentMain = document.createElement("div");
-  contentMain.innerHTML = "<h1>estas en CHAT INDIVIDUAL</h1><p>Loreeem exampleeeeeeee page!</p>";
-  return contentMain;
-}
-
