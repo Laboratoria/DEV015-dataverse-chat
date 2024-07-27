abce99175bb650536105287daeefe9eeec72fb2a
Angie <angiesamper18@gmail.com>
2024-07-27 13:19:10 -0400

    merge

--- src/router.js
+++ src/router.js
@@ -3,16 +3,13 @@ let ROUTES = {};
 let rootEl;
 
 export const setRootEl = (el) => {
-  // assign rootEl
   rootEl = el;
 
   return rootEl;
 }
 
 export const setRoutes = (routes) => {
-  // optional Throw errors if routes isn't an object
-  // optional Throw errors if routes doesn't define an /error route
-  // assign ROUTES
+  
   if(typeof routes === "object"){
     if(routes['/error']){
       ROUTES = routes;
@@ -29,13 +26,11 @@ export const queryStringToObject = (queryString) => {
 
 export const renderView = (pathname, props={}) => { //props son los search params
 
-  // clear the root element
   rootEl.innerHTML = ""
   const viewPath = ROUTES[location.pathname];
 
   if(!viewPath){
     navigateTo("/error");
-    // throw new Error("Invalid viewPath");
     return;
   }
   const component = viewPath(props) 
@@ -44,28 +39,19 @@ export const renderView = (pathname, props={}) => { //props son los search param
 }
 
 export const navigateTo = (pathname, props = {}) => {
-  // Esta función se utiliza para navegar mediante programación a una nueva ruta dentro del SPA (por ejemplo, hacer clic en botones o enlaces dentro de la aplicación). Actualiza la URL usando window.history.pushState y luego llama a renderView él pathname y los prop usdados para mostrar la vista correspondiente.
-  // update window history with WINDOW.HISTORY.pushState
-  
+ 
   const url = `${window.location.origin}${pathname}${props ? `?${new URLSearchParams(props)}` : "" }` 
   window.history.pushState({}, '', url );
   if (window.history && window.history.pushState) {
     renderView(pathname, props)
   }
   
-  // render the view passing props
   renderView(pathname, props);
-  // Nota: ya hablamos de una función renderView, podemos usarla dentro de navigateTo para mantener el código DRY (Don't Repeat Yourself, No te repitas)
+  
 };
 export const onURLChange = (location) => {
   
-  // parse the location for the pathname and search params
-  
   const { pathname, search } = location;
-  // console.log('este es el pathname --->' + pathname)
-  // console.log('este es el search --->' + search)
-  // convert the search params to an object
   const props = queryStringToObject(search);
-  // render the view with the pathname and object
   renderView(pathname, props);
 };
