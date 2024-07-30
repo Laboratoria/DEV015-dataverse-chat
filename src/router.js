b3bb4b8c6f1a85f79f258f1161dd81fbea781cbb
fe-ercg <mabfher@gmail.com>
2024-07-30 09:17:28 -0500

    test: router test

--- src/router.js
+++ src/router.js
@@ -1,4 +1,3 @@
-
 let ROUTES = {};
 let rootEl;
 
@@ -7,60 +6,63 @@ export const setRootEl = (el) => {
   rootEl = el;
 
   return rootEl;
-}
+};
 
 export const setRoutes = (routes) => {
   // optional Throw errors if routes isn't an object
-  // optional Throw errors if routes doesn't define an /error route
-  // assign ROUTES
-  if(typeof routes === "object"){
-    if(routes['/error']){
-      ROUTES = routes;
-    }
+  if (typeof routes !== "object") {
+    throw new Error("no es un objeto");
   }
+  // optional Throw errors if routes doesn't define an /error route
+  if (!routes["/error"]) {
+    throw new Error("la vista de error no existe");
+  }
+  // assign ROUTES
+  ROUTES = routes;
+
   return ROUTES;
-}
+};
 
 export const queryStringToObject = (queryString) => {
   const urlSearchParams = new URLSearchParams(queryString);
   return Object.fromEntries(urlSearchParams);
-  
-}
+};
 
-export const renderView = (pathname, props={}) => { //props son los search params
+export const renderView = (pathname, props = {}) => {
+  //props son los search params
 
   // clear the root element
-  rootEl.innerHTML = ""
+  rootEl.innerHTML = "";
   const viewPath = ROUTES[location.pathname];
 
-  if(!viewPath){
+  if (!viewPath) {
     navigateTo("/error");
     // throw new Error("Invalid viewPath");
     return;
   }
-  const component = viewPath(props) 
+  const component = viewPath(props);
   rootEl.append(component);
-
-}
+};
 
 export const navigateTo = (pathname, props = {}) => {
   // Esta función se utiliza para navegar mediante programación a una nueva ruta dentro del SPA (por ejemplo, hacer clic en botones o enlaces dentro de la aplicación). Actualiza la URL usando window.history.pushState y luego llama a renderView él pathname y los prop usdados para mostrar la vista correspondiente.
   // update window history with WINDOW.HISTORY.pushState
-  
-  const url = `${window.location.origin}${pathname}${props ? `?${new URLSearchParams(props)}` : "" }` 
-  window.history.pushState({}, '', url );
+
+  const url = `${window.location.origin}${pathname}${
+    props ? `?${new URLSearchParams(props)}` : ""
+  }`;
+  window.history.pushState({}, "", url);
   if (window.history && window.history.pushState) {
-    renderView(pathname, props)
+    renderView(pathname, props);
   }
-  
+
   // render the view passing props
   renderView(pathname, props);
   // Nota: ya hablamos de una función renderView, podemos usarla dentro de navigateTo para mantener el código DRY (Don't Repeat Yourself, No te repitas)
 };
 export const onURLChange = (location) => {
-  
   // parse the location for the pathname and search params
-  
+
   const { pathname, search } = location;
   // console.log('este es el pathname --->' + pathname)
   // console.log('este es el search --->' + search)
