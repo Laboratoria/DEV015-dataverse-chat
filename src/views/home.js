f824f4db7011a4b9f93416f98c77f000826564cb
danimar-g <danielamar.gerard@gmail.com>
2024-08-14 18:34:16 -0400

    intento de ver tarjeta

--- src/views/home.js
+++ src/views/home.js
@@ -1,8 +1,17 @@
+import { data } from '../data/dataset.js';
+import { filterData } from '../lib/dataFunctions.js';
+
 export const Home = () => {
     const el = document.createElement("h1");
     el.innerHTML = "Dreamland";
 
-
+  const renderItems = (data) => {
+    const ulElement = document.createElement('ul');
+    data.forEach(item =>
+      ulElement.appendChild(Card(item))
+    );
+  return ulElement;
+  };
     
    return el;
  }
