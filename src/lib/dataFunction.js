6a5e3166abb447cf36c25df64d4c4657ce576d46
lisettevirginia <lisavirginia@gmail.com>
2024-08-19 13:53:58 -0500

    Se agregaron las funciones del dataverse, y se hicieron las pruebas unitarias, todo funciona ok

--- src/lib/dataFunction.js
+++ src/lib/dataFunction.js
@@ -1,11 +1,52 @@
 export const filterData = (data, filterBy, value) => {
-  return 'example';
+  return data.filter(character => 
+    character.facts[filterBy].toLowerCase() === value.toLowerCase()
+  );
 };
   
+  
 export const sortData = (data, sortBy, sortOrder) => {
-  return [];
+  // exportamos la función para ordenar personajes por nombre
+  //data:array de objeto(personajes),sortBy:campo del objeto,sortOrder: asc-des
+  const sortedCharacters = [...data];
+  //crea un nuevo array(copia)que contiene todos los elementos del array 
+  return sortedCharacters.sort((a, b) => {
+    //compara cada par de elementos y luego retorna el array ordenado. 
+    if (sortOrder === 'asc') {
+    //si es excatamente igual se ordena de forma ascendente
+      return a[sortBy].localeCompare(b[sortBy]);
+    //método de las cadenas de texto en JS que compara dos cadenas de texto
+    } else if (sortOrder === 'des') {
+    //ademas si ...
+      return b[sortBy].localeCompare(a[sortBy]);
+      //comparacion a la inversa (desendente)
+
+    }
+  });
 };
   
 export const computeStats = (data) => {
-  return [];
+  return data.reduce((estadisticas, character) => {
+    // Función para calcular estadísticas usando reduce
+    const sitSentimental = character.facts.sitSentimental.toLowerCase();
+    //accede a la propiedad sitSentimental de un objeto character 
+    //y almacena el resultado en la constante sitSentimental
+    
+    if (sitSentimental.includes("casad")) {
+      estadisticas.casados++;
+    } else if (sitSentimental.includes("solter")) {
+      estadisticas.solteros++;
+    } else if (sitSentimental.includes("viud")) {
+      estadisticas.viudos++;
+    } else if (sitSentimental.includes("amante")) {
+      estadisticas.amantes++;
+    }
+    return estadisticas;
+  }, {
+    casados: 0,
+    solteros: 0,
+    viudos: 0,
+    amantes: 0
+    //contador con el que inicializan los contadores
+  });
 };
---
+++ test/dataFunction.spec.js
@@ -0,0 +1,50 @@
+// test/dataFunctions.spec.js
+import data from '../src/data/dataset.js'; //importamos el dataset
+import { filterData } from '../src/lib/dataFunction.js';
+import { sortData } from '../src/lib/dataFunction.js';
+import { computeStats } from '../src/lib/dataFunction.js';
+
+describe('filterData', () => {
+  it('returns characters from the Bridgerton family', () => {
+    const result = filterData(data, 'familia', 'Bridgerton'); // Filtra la familia "Bridgerton": Utiliza filterData para obtener todos los personajes que pertenecen a la familia "Bridgerton".
+    const expectedCharacters = data.filter(character => character.facts.familia === 'Bridgerton'); // Esto filtra el dataset original para obtener el array de personajes que coinciden con la familia "Bridgerton" y compara con el resultado de filterData
+    expect(result).toEqual(expectedCharacters); // Compara ambos resultados
+  });
+
+  it('returns an empty array if no characters match the family', () => { //Filtra la familia "Featherington": Similar a la primera prueba, pero busca personajes de la familia "Featherington".
+    const result = filterData(data, 'familia', 'Featherington'); 
+    const expectedCharacters = data.filter(character => character.facts.familia === 'Featherington'); //Si ningún personaje coincide con la familia, se espera que la función retorne un array vacío.
+
+    expect(result).toEqual(expectedCharacters);
+  });
+});
+
+const getExpectedSortedData = (data, order) => {
+  return [...data].sort((a, b) => {
+    if (order === 'asc') return a.name.localeCompare(b.name);
+    if (order === 'des') return b.name.localeCompare(a.name);
+  });
+};
+
+describe('sortData', () => {
+  test('should sort data by name in ascending order', () => {
+    expect(sortData(data, 'name', 'asc')).toEqual(getExpectedSortedData(data, 'asc'));
+  });
+
+  test('should sort data by name in descending order', () => {
+    expect(sortData(data, 'name', 'des')).toEqual(getExpectedSortedData(data, 'des'));
+  });
+});
+
+describe('computeStats', () => {
+  test('should compute correct statistics for sentiment status', () => {
+    const result = computeStats(data);
+    const expected = {
+      casados: 13,   
+      solteros: 5,  
+      viudos: 4,    
+      amantes: 2,    
+    };
+    expect(result).toEqual(expected);
+  });
+});
