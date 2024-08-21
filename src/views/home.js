dad77ebe2ccfc4069b6e5d136c3f30173c0bfea2
danimar-g <danielamar.gerard@gmail.com>
2024-08-21 13:31:05 -0400

    homi

--- src/views/home.js
+++ src/views/home.js
@@ -1,29 +1,15 @@
-import  data  from '../data/dataset.js';
-import { filterData } from '../lib/dataFunctions.js';
-import { card } from '../Components/Card.js';
-
-export const Home = () => {
+const Home = () => {
     const el = document.createElement("h1");
     el.innerHTML = "Dreamland";
-
-  const div = document.createElement('div');
-  div.appendChild(el)
-    
-  const showCard = (data) => {
-    const ulElement = document.createElement('ul');
-    
-    data.forEach(item => {
-      /*const li = document.createElement('li')
-      li.innerHTML = item.id */
-      ulElement.appendChild(card(item))}
-  
-  );
-  return ulElement;
-  };
-   div.appendChild(showCard(data)) 
-   return div;
+   return el;
  }
 
+ //funcion para crear tarjetas
+ const renderItems = (data) => {
+    const ulElement = document.createElement('ul');
+    data.forEach(pelicula => 
 
-
- export default Home;
+    ulElement.appenChild(Card("ulPelis"))
+    );
+    return ulElement;
+ };
