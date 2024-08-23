947994c54a27ce65562fbbcee80ddc0efb486e8e
avsotomejia@gmail.com <avsotomejia@gmail.com>
2024-08-22 23:09:46 -0500

    Creación de archivos

--- src/index.js
+++ src/index.js
@@ -1,19 +1,16 @@
 // En este archivo definirás tus rutas e importarás los componentes que vas a renderizar.
 
 /*
-import Example from './views/Example.js';
-
-Ejemplo de definición de rutas:
-
-const routes = {
-    "/": Example,
-    ...
-}
-*/
-
-/*
 TODO:
 1.- Definir rutas en router.
 2.- Pasar "root element" a router.
 3.- Invocar el router para renderizar la vista correcta.
 */
+
+// Definir rutas en router
+const routes = {
+  "/": Home,
+};
+
+const container = document.getElementById("root");
+
