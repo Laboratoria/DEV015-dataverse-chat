1cee32a55b611204763730101c4b0617b4982f9e
danimar-g <danielamar.gerard@gmail.com>
2024-08-28 21:39:30 -0400

    arreglo errores

--- src/Components/filtroyorden.js
+++ src/Components/filtroyorden.js
@@ -1,46 +1,65 @@
-import { filterData, sortData } from "../lib/dataFunctions";
+//import { filterData, sortData } from "../lib/dataFunctions";
+import { card } from "./Card";
+export const filtroyorden = () => {
+  const div = document.createElement("div");
+  div.innerHTML = `<label for="epoca"><span class="tituloSelectores"><b> Selecciona por épocas</b></span></label>
+<select id="epoca" data-testid="select-filter" name="epocas">
+<option value="0">Seleccionar</option>
+<option value="1990">1990</option>
+<option value="2000">2000</option>
+<option value="2010">2010</option>
+<option value="2020">2020</option>
+</select>
 
-const selectoresEpoca = document.querySelector("#epoca");
-const selectoresOrden = document.querySelector("#nombre");
+<label for="nombre"><span class="tituloSelectores"><b>Ordena por nombre</b></span></label>
+<select id="nombre" data-testid="select-sort" name="name">
+  <option value="orden">Ordenar</option>
+  <option value="Asc">A - Z</option>
+  <option value="Desc">Z - A</option>
+</select>
 
+<button id="borrar" data-testid="button-clear">Limpiar Filtros</button>`;
 
-selectoresEpoca.addEventListener("change", (event) => { 
+  const selectoresEpoca = div.querySelector("#epoca");
+
+  const selectoresOrden = div.querySelector("#nombre");
+
+  selectoresEpoca.addEventListener("change", (event) => {
     const valor = event.target.value;
     let dataFiltrada;
     if (valor === "0") {
       dataFiltrada = data;
     } else {
-      dataFiltrada = filterData(data, "estreno", valor)
+      //   dataFiltrada = filterData(data, "estreno", valor)
     }
-    
+
     const valorOrden = document.getElementById("nombre").value;
     if (valorOrden !== "orden") {
-      dataFiltrada = sortData(dataFiltrada, "name", valorOrden);
+      // dataFiltrada = sortData(dataFiltrada, "name", valorOrden);
     }
-  
-    
-    const listaFiltrada = renderItems(dataFiltrada);
+
+    const listaFiltrada = card(dataFiltrada);
     elementos.innerHTML = "";
     elementos.appendChild(listaFiltrada);
-  
   });
-  
+
   selectoresOrden.addEventListener("change", (e) => {
     const value = e.target.value;
     let dataOrdenada;
     if (value === "orden") {
       dataOrdenada = data;
-    }  else {
-      dataOrdenada = sortData(data, "name", value);
+    } else {
+      // dataOrdenada = sortData(data, "name", value);
     }
-  
+
     const valorFiltro = document.getElementById("epoca").value;
     if (valorFiltro !== "0") {
-      dataOrdenada = filterData(dataOrdenada, "estreno", valorFiltro);
+      //dataOrdenada = filterData(dataOrdenada, "estreno", valorFiltro);
     }
-    
-    const listaOrdenada = renderItems(dataOrdenada);
+
+    const listaOrdenada = card(dataOrdenada);
     elementos.innerHTML = "";
     elementos.appendChild(listaOrdenada);
   });
-  
+  return div;
+};
--- src/views/home.js
+++ src/views/home.js
@@ -2,7 +2,7 @@ import  data  from "../data/dataset.js";
 import { card } from "../Components/Card.js";
 import { createHeader } from "../components/header.js"; 
 import { createFooter } from "../Components/footer.js";
-//import {filterData, sortData} from "../Components/filtroyorden.js";
+import {filtroyorden} from "../Components/filtroyorden.js";
 
 export const Home = () => {
   const div = document.createElement("div");
@@ -14,8 +14,7 @@ export const Home = () => {
   subtitle.innerHTML = "¡Bienvenidos fans de Disney y Pixar! Explora nuestro mágico contenido y descubre las novedades de este fantástico mundo donde puedes chatear con tus películas favoritas";
   div.appendChild(subtitle);
 
-  /*const sortSelector = document.createElement("select");
-  sortSelector.id = "Nombre";*/
+div.appendChild(filtroyorden());
 
   const showCard = (data) => {
     const ulElement = document.createElement("ul");
